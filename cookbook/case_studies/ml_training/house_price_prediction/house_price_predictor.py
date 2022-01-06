669086def4e170b6adfd2dc94a4cdff1f147116c
SmritiSatyanV <smriti@union.ai>
2022-01-06 14:31:32 +0530

    Revert "[wip]Predicting House Price"
    This reverts commit 168190147c0adb3016c77217b382f7997b1eaad4.

--- cookbook/case_studies/ml_training/house_price_prediction/house_price_predictor.py
+++ cookbook/case_studies/ml_training/house_price_prediction/house_price_predictor.py
@@ -1,11 +1,10 @@
 """
 Predicting House Price in a Region with XGBoost
 ------------------------------------------------
-
 """
 
 # %%
-# Install the following libraries before running the model (locally):
+# Install the following three libraries before running the model (locally):
 #
 # .. code-block:: python
 #
@@ -16,8 +15,7 @@ Predicting House Price in a Region with XGBoost
 # %%
 # Importing the Libraries
 # ========================
-#
-# First, let's import the required packages into the environment.
+# First, import all the required libraries.
 import typing
 
 import os
@@ -32,7 +30,9 @@ from flytekit.types.file import JoblibSerializedFile
 from typing import Tuple
 
 # %%
-# We initialize variables that represent columns in the dataset. We will use these variables to build the model.
+# Initializing the Variables
+# ===========================
+# Initialize the variables to be used while building the model.
 NUM_HOUSES_PER_LOCATION = 1000
 COLUMNS = [
     "PRICE",
@@ -44,14 +44,12 @@ COLUMNS = [
     "GARAGE_SPACES",
 ]
 MAX_YEAR = 2021
-# Now, we divide the data into train, validation, and test datasets in specific ratio.
 SPLIT_RATIOS = [0.6, 0.3, 0.1]
 
 # %%
-# Data Generation
+# Defining the Data Generation Functions
 # =======================================
-#
-# We define a function that generates the price of a house based on multiple factors (such as `number of bedrooms`, `number of bathrooms`, `area`, `garage space` and `year built`).
+# Define a function to generate the price of a house.
 def gen_price(house) -> int:
     _base_price = int(house["SQUARE_FEET"] * 150)
     _price = int(
@@ -66,7 +64,7 @@ def gen_price(house) -> int:
 
 
 # %%
-# Now, let's generate a DataFrame object that constitutes all the houses' details.
+# Define a function that returns a DataFrame object constituting all the houses' details.
 def gen_houses(num_houses) -> pd.DataFrame:
     _house_list = []
     for _ in range(num_houses):
@@ -79,7 +77,6 @@ def gen_houses(num_houses) -> pd.DataFrame:
             "YEAR_BUILT": min(MAX_YEAR, int(np.random.normal(1995, 10))),
         }
         _price = gen_price(_house)
-        # column names/features 
         _house_list.append(
             [
                 _price,
@@ -91,7 +88,6 @@ def gen_houses(num_houses) -> pd.DataFrame:
                 _house["GARAGE_SPACES"],
             ]
         )
-    # convert the list to a DataFrame    
     _df = pd.DataFrame(
         _house_list,
         columns=COLUMNS,
@@ -100,7 +96,7 @@ def gen_houses(num_houses) -> pd.DataFrame:
 
 
 # %%
-# We split the data into train, validation, and test datasets.
+# Split the data into train, val, and test datasets.
 def split_data(
     df: pd.DataFrame, seed: int, split: typing.List[float]
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
@@ -110,16 +106,16 @@ def split_data(
     test_size = split[2]
 
     num_samples = df.shape[0]
-    # retain the features, skip the target column
-    x1 = df.values[:num_samples, 1:]  
-    # retain the target column
-    y1 = df.values[:num_samples, :1]  
+    x1 = df.values[
+        :num_samples, 1:
+    ]  # keep only the features, skip the target, all rows
+    y1 = df.values[:num_samples, :1]  # keep only the target, all rows
 
-    # divide the input into train and test data
+    # Use split ratios to divide up into train & test
     x_train, x_test, y_train, y_test = train_test_split(
         x1, y1, test_size=test_size, random_state=seed
     )
-    # give proper ratio to train and validation data in the remaining data 
+    # Of the remaining training samples, give proper ratio to train & validation
     x_train, x_val, y_train, y_val = train_test_split(
         x_train,
         y_train,
@@ -127,12 +123,11 @@ def split_data(
         random_state=seed,
     )
 
-    # reassemble the datasets by placing `target` as first column and `features` in subsequent columns
+    # Reassemble the datasets with target in first column and features after that
     _train = np.concatenate([y_train, x_train], axis=1)
     _val = np.concatenate([y_val, x_val], axis=1)
     _test = np.concatenate([y_test, x_test], axis=1)
 
-    # return three DataFrames with train, test, and validation data
     return (
         pd.DataFrame(
             _train,
@@ -150,10 +145,9 @@ def split_data(
 
 
 # %%
-# Defining a Task to generate and split the input dataset
-# =============================================================
-#
-# We define a task to generate a DataFrame with house details. It will return three DataFrames with train, test, and validation data.
+# Task: Generating & Splitting the Data
+# ======================================
+# Call the previously defined helper functions to generate and split the data. Finally, return the DataFrame objects.
 dataset = typing.NamedTuple(
     "GenerateSplitDataOutputs",
     train_data=pd.DataFrame,
@@ -169,23 +163,21 @@ def generate_and_split_data(number_of_houses: int, seed: int) -> dataset:
 
 
 # %%
-# Defining a Task to train the XGBoost model
-# ===========================================
-#
-# Now, we define another task to serialize the XGBoost model using `joblib` and store the model in a `dat` file.
+# Task: Training the XGBoost Model
+# =================================
+# Serialize the XGBoost model using joblib and store the model in a dat file.
 @task(cache_version="1.0", cache=True, limits=Resources(mem="600Mi"))
 def fit(loc: str, train: pd.DataFrame, val: pd.DataFrame) -> JoblibSerializedFile:
 
-    # fetch the features and target columns from the train dataset
+    # Fetch the input and output data from train dataset
     x = train[train.columns[1:]]
     y = train[train.columns[0]]
 
-    # fetch the features and target columns from the validation dataset
+    # Fetch the input and output data from validation dataset
     eval_x = val[val.columns[1:]]
     eval_y = val[val.columns[0]]
 
     m = XGBRegressor()
-    # fit the model to the train data
     m.fit(x, y, eval_set=[(eval_x, eval_y)])
 
     working_dir = flytekit.current_context().working_directory
@@ -195,26 +187,24 @@ def fit(loc: str, train: pd.DataFrame, val: pd.DataFrame) -> JoblibSerializedFil
 
 
 # %%
-# Defining a Task to forecast house prices
-# =========================================
-#
-# We define one last task to unserialize the XGBoost model using `joblib` to generate the predictions.
+# Task: Generating the Predictions
+# ===================================
+# Unserialize the XGBoost model using joblib and generate the predictions.
 @task(cache_version="1.0", cache=True, limits=Resources(mem="600Mi"))
 def predict(
     test: pd.DataFrame,
     model_ser: JoblibSerializedFile,
 ) -> typing.List[float]:
 
-    # load the model
+    # Load model
     model = joblib.load(model_ser)
 
-    # load the test data
+    # Load test data
     x_df = test[test.columns[1:]]
 
-    # generate predictions
+    # Generate predictions
     y_pred = model.predict(x_df).tolist()
 
-    # return the predictions
     return y_pred
 
 
@@ -231,7 +221,7 @@ def house_price_predictor_trainer(
     seed: int = 7, number_of_houses: int = NUM_HOUSES_PER_LOCATION
 ) -> typing.List[float]:
 
-    # generate and split the data
+    # Generate and split the data
     split_data_vals = generate_and_split_data(
         number_of_houses=number_of_houses, seed=seed
     )
@@ -241,7 +231,7 @@ def house_price_predictor_trainer(
         loc="NewYork_NY", train=split_data_vals.train_data, val=split_data_vals.val_data
     )
 
-    # generate predictions
+    # Generate predictions
     predictions = predict(model_ser=model, test=split_data_vals.test_data)
 
     return predictions
